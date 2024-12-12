3b771eaa8eb9b65c9d59d1c13d6f5fbd86b01dd0
Roadgar <roadgar2@gmail.com>
2024-12-12 15:53:25 +0100

    Update AppFooter.vue

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,19 +1,1 @@
-<template>
-  <footer class="footer bg-dark text-white py-4">
-    <div class="container">
-      <p class="text-center mb-0">© 2024 CarHomes. Todos los derechos reservados.</p>
-      <p class="text-center mb-0">
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener" class="text-white">fghdfhdhdh</a> |
-        <a href="https://github.com/vuejs" target="_blank" rel="noopener" class="text-white">hdfhdghdgh</a> |
-        <a href="https://www.vuejs.org" target="_blank" rel="noopener" class="text-white">dghdghdghdgh</a>
-      </p>
-    </div>
-  </footer>
-</template>
 
-<style scoped>
-.footer {
-  width: 100%;
-  position: relative; 
-}
-</style>
